bb9c53944414fb92fdaeed3da9b0ba98db86c57b
bahamuchomax-dev <bahamuchomax@gmail.com>
2026-03-03 15:45:02 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const config = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "genro-b74de.firebaseapp.com",
   projectId: "genro-b74de",
   storageBucket: "genro-b74de.firebasestorage.app",
   messagingSenderId: "311645846310",
   appId: "1:311645846310:web:4a11cadf49825db1f55fe7",
 };
-
+// 本当にキーが届いているか、コンソールに出力して確認
+console.log("設定されているAPIキー:", config.apiKey);
 const app = getApps().length === 0 ? initializeApp(config) : getApp();
 export const db = getFirestore(app);
 export const auth = getAuth(app);
